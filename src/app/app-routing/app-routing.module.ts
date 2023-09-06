a256a979373edc7270a726111ae34ed4b43c8ef4
Jordan <jordan@Jordans-MacBook-Air.local>
2023-09-06 10:40:36 -0600

    set up routing

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from '../components/about-me/about-me.component';
+import { ExperienceComponent } from '../components/experience/experience.component';
+import { PortfolioComponent } from '../components/portfolio/portfolio.component';
+
+const routes: Routes = [
+  // {path: '', redirectTo: 'about-me', pathMatch: 'full'},
+  {path:'about-me', component: AboutMeComponent},
+  {path:'experience', component: ExperienceComponent},
+  {path:'portfolio', component: PortfolioComponent}
+]
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    AboutMeComponent,
+    ContactComponent,
+    ExperienceComponent,
+    TopNavComponent,
+    SideNavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
