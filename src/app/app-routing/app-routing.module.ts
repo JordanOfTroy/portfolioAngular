b8eff0d028eb712b29a2afad7848a7c2de0c719a
Jordan <jordan@Jordans-MacBook-Air.local>
2023-09-07 07:37:10 -0600

    added contact me to routing

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from '../components/about-me/about-me.component';
 import { ExperienceComponent } from '../components/experience/experience.component';
 import { PortfolioComponent } from '../components/portfolio/portfolio.component';
+import { ContactComponent } from '../components/contact/contact.component';
 
 const routes: Routes = [
-  // {path: '', redirectTo: 'about-me', pathMatch: 'full'},
+  {path: '', redirectTo: 'about-me', pathMatch: 'full'},
   {path:'about-me', component: AboutMeComponent},
   {path:'experience', component: ExperienceComponent},
-  {path:'portfolio', component: PortfolioComponent}
+  {path:'portfolio', component: PortfolioComponent},
+  {path:'contact', component: ContactComponent}
 ]
 
 @NgModule({
