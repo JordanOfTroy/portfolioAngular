7f8907f4c78534d701a856ee0975d23fed897977
Jordan <jordan@Jordans-MacBook-Air.local>
2023-09-20 10:18:42 -0600

    added list of real jobs

---
+++ environment.ts
@@ -0,0 +1,67 @@
+export const environment = {
+    prevProjects: [
+        {
+            name: 'Barbra Eaton',
+            url: 'https://drbarbaraeaton.com/',
+            imageURL: '',
+            description: '',
+            stack: ['Wordpress']
+        },
+        {
+            name: 'Mariners Museum',
+            url: 'https://www.marinersmuseum.org/',
+            imageURL: '',
+            description: '',
+            stack: ['Wordpress']
+        },
+        {
+            name: 'Hooke Audio',
+            url: 'https://hookeaudio.com/',
+            imageURL: '',
+            description: '',
+            stack: ['Wordpress']
+        },
+        {
+            name: 'Nexant Group',
+            url: 'https://wattsmartsavings.net/',
+            imageURL: '',
+            description: '',
+            stack: ['Wordpress']
+        },
+        {
+            name: 'Paul Neustein',
+            url: 'https://drneustein.com/',
+            imageURL: '',
+            description: '',
+            stack: ['Wordpress']
+        },
+        {
+            name: 'Limitless Landscaping',
+            url: 'https://limitlesslandscape.net/',
+            imageURL: '',
+            description: '',
+            stack: ['Wordpress']
+        },
+        {
+            name: 'Story Tree',
+            url: 'http://storytree-dev.herokuapp.com/',
+            imageURL: '',
+            description: '',
+            stack: ['Python|DMP (Django Mako Plus)']
+        },
+        {
+            name: 'Scripture Notes',
+            url: 'https://scripturenotes.com/',
+            imageURL: '',
+            description: '',
+            stack: ['Javascript|React', 'Node']
+        },
+        {
+            name: 'Compliance Group - Site tool',
+            url: 'https://compliancego.com/',
+            imageURL: '',
+            description: '',
+            stack: ['Javascript|React', 'Node']
+        }
+    ]
+}
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Project } from 'src/app/interfaces/project';
+import { environment } from 'environment';
 
 @Component({
   selector: 'app-portfolio',
@@ -8,29 +9,31 @@ import { Project } from 'src/app/interfaces/project';
 })
 export class PortfolioComponent {
 
-  testProjects = [
-    {
-      name: 'Test Project 1',
-      description: "That's it mister! you just lost your brain priviliges! i'm so cold, i can use my nose drippings as chopsticks. you mean what we thought they thought we think and thought they thought? isn't this great squidward? it's just the 3 of us. you, me, and this brick wall you built between us. you're a man now, spongebob, and it's time you started acting like one. ",
-      url: "https://panipsum.com/",
-      imageURL: 'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-10100201-scaled.jpg',
-      stack: ['Javascript', 'SASS', 'React', 'Node', 'SQL/Postgres']
-    },
-    {
-      name: 'Test Project 2',
-      description: "That's it mister! you just lost your brain priviliges! i'm so cold, i can use my nose drippings as chopsticks. you mean what we thought they thought we think and thought they thought? isn't this great squidward? it's just the 3 of us. you, me, and this brick wall you built between us. you're a man now, spongebob, and it's time you started acting like one. ",
-      url: "https://panipsum.com/",
-      imageURL: 'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-10100201-scaled.jpg',
-      stack: ['Javascript', 'SASS', 'Angular', 'Firebase']
-    },
-    {
-      name: 'Test Project 3',
-      description: "That's it mister! you just lost your brain priviliges! i'm so cold, i can use my nose drippings as chopsticks. you mean what we thought they thought we think and thought they thought? isn't this great squidward? it's just the 3 of us. you, me, and this brick wall you built between us. you're a man now, spongebob, and it's time you started acting like one. ",
-      url: "https://panipsum.com/",
-      imageURL: 'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-10100201-scaled.jpg',
-      stack: ['Javascript', 'SASS', 'React', 'Node', 'SQL/Postgres']
-    },
-  ]
+  // testProjects = [
+  //   {
+  //     name: 'Test Project 1',
+  //     description: "That's it mister! you just lost your brain priviliges! i'm so cold, i can use my nose drippings as chopsticks. you mean what we thought they thought we think and thought they thought? isn't this great squidward? it's just the 3 of us. you, me, and this brick wall you built between us. you're a man now, spongebob, and it's time you started acting like one. ",
+  //     url: "https://panipsum.com/",
+  //     imageURL: 'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-10100201-scaled.jpg',
+  //     stack: ['Javascript', 'SASS', 'React', 'Node', 'SQL/Postgres']
+  //   },
+  //   {
+  //     name: 'Test Project 2',
+  //     description: "That's it mister! you just lost your brain priviliges! i'm so cold, i can use my nose drippings as chopsticks. you mean what we thought they thought we think and thought they thought? isn't this great squidward? it's just the 3 of us. you, me, and this brick wall you built between us. you're a man now, spongebob, and it's time you started acting like one. ",
+  //     url: "https://panipsum.com/",
+  //     imageURL: 'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-10100201-scaled.jpg',
+  //     stack: ['Javascript', 'SASS', 'Angular', 'Firebase']
+  //   },
+  //   {
+  //     name: 'Test Project 3',
+  //     description: "That's it mister! you just lost your brain priviliges! i'm so cold, i can use my nose drippings as chopsticks. you mean what we thought they thought we think and thought they thought? isn't this great squidward? it's just the 3 of us. you, me, and this brick wall you built between us. you're a man now, spongebob, and it's time you started acting like one. ",
+  //     url: "https://panipsum.com/",
+  //     imageURL: 'https://www.rd.com/wp-content/uploads/2021/04/GettyImages-10100201-scaled.jpg',
+  //     stack: ['Javascript', 'SASS', 'React', 'Node', 'SQL/Postgres']
+  //   },
+  // ]
+
+  testProjects = environment.prevProjects
 
 
 
