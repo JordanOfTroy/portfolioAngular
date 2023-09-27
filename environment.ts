f2eb88351b2d378292c51693db1cff8acdcbc73f
Jordan <jordan@Jordans-MacBook-Air.local>
2023-09-27 09:35:27 -0600

    added cropped imaged and css to portfolio

--- environment.ts
+++ environment.ts
@@ -5,90 +5,150 @@ export const environment = {
             name: 'Barbra Eaton',
             url: 'https://drbarbaraeaton.com/',
             images: [
-                'assets/rawImages/screencapture-drbarbaraeaton-2023-09-27-07_37_56.png',
+                'assets/uniformImages/screencapture-drbarbaraeaton-2023-09-27-07_37_56.png',
             ],
-            description: '',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Wordpress']
         },
         {
             name: 'Epic Party Events',
             url: 'https://epicpartyevents.com/',
             images: [
-                'assets/rawImages/screencapture-epicpartyevents-2023-09-27-07_45_19.png',
+                'assets/uniformImages/screencapture-epicpartyevents-2023-09-27-07_45_19.png',
             ],
-            description: 'stupid fucking project',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Wordpress']
         },
         {
             name: 'Mariners Museum',
             url: 'https://www.marinersmuseum.org/',
             images: [
-                'assets/rawImages/screencapture-marinersmuseum-org-2023-09-27-07_39_17.png'
+                'assets/uniformImages/screencapture-marinersmuseum-org-2023-09-27-07_39_17.png'
             ],
-            description: '',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Wordpress']
         },
         {
             name: 'Hooke Audio',
             url: 'https://hookeaudio.com/',
             images: [
-                'assets/rawImages/screencapture-hookeaudio-2023-09-27-07_39_59.png'
+                'assets/uniformImages/screencapture-hookeaudio-2023-09-27-07_39_59.png'
             ],
-            description: '',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Wordpress']
         },
         {
             name: 'Nexant Group',
             url: 'https://wattsmartsavings.net/',
             images: [
-                'assets/rawImages/screencapture-Nexant-wattsmartsavings-net-2023-09-27-07_40_56.png'
+                'assets/uniformImages/screencapture-Nexant-wattsmartsavings-net-2023-09-27-07_40_56.png'
             ],
-            description: '',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Wordpress']
         },
         {
             name: 'Paul Neustein',
             url: 'https://drneustein.com/',
             images: [
-                'assets/rawImages/screencapture-drneustein-2023-09-27-07_41_59.png'
+                'assets/uniformImages/screencapture-drneustein-2023-09-27-07_41_59.png'
             ],
-            description: '',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Wordpress']
         },
         {
             name: 'Limitless Landscaping',
             url: 'https://limitlesslandscape.net/',
             images: [
-                'assets/rawImages/screencapture-limitlesslandscape-net-2023-09-27-07_43_14.png'
+                'assets/uniformImages/screencapture-limitlesslandscape-net-2023-09-27-07_43_14.png'
             ],
-            description: '',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Wordpress']
         },
         {
             name: 'Story Tree',
             url: 'http://www.storytree.com/',
             images: [
-                'assets/rawImages/screencapture-storytree-2023-09-27-07_47_17.png'
+                'assets/uniformImages/screencapture-storytree-2023-09-27-07_47_17.png'
             ],
-            description: '',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Python|DMP (Django Mako Plus)']
         },
         {
             name: 'Scripture Notes',
             url: 'https://scripturenotes.com/',
             images: [
-                'assets/rawImages/screencapture-scripturenotes-2023-09-27-07_48_59.png'
+                'assets/uniformImages/screencapture-scripturenotes-2023-09-27-07_48_59.png'
             ],
-            description: '',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Javascript|React', 'Node']
         },
         {
             name: 'Compliance Group - Site tool',
             url: 'https://compliancego.com/',
             images: [
-                'assets/rawImages/screencapture-compliancego-2023-09-27-07_49_47.png'
+                'assets/uniformImages/screencapture-compliancego-2023-09-27-07_49_47.png'
             ],
-            description: '',
+            description: `
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+
+             I'm so cold, i can use my nose drippings as chopsticks. squidward? squidward! the sky had a baby from my cereal box! good people don't rip other people's arms off. plankton: 1% evil, 99% hot gas. can't have dirty garbage! did i patrick? did i? or did your criminal mind hypnotize me to steal it? there once was a man from peru who dreamed he was eating his shoe.
+            `,
             stack: ['Javascript|React', 'Node']
         }
     ],
